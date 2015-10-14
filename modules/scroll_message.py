208abffa5f788b9e03de3b29577f54fb975cdea3
Daniel Kurdoghlian <daniel@pushingpixels.at>
2015-10-14 20:54:10 +0200

    custom bitmap fonts - now let's use them

--- modules/scroll_message.py
+++ modules/scroll_message.py
@@ -1,9 +1,6 @@
 import time
-import pygame
 
-from PIL import ImageFont
-from PIL import Image
-from PIL import ImageDraw
+from fonts.font import Proportional, DEFAULT_FONT
 
 from module import Module
 from settings import *
@@ -15,12 +12,51 @@ class ScrollMessage(Module):
         super(ScrollMessage, self).__init__(screen)
 
         self.text = text
-        self.font = ImageFont.truetype("/home/repos/git/pixelpi/fonts/freesansbold.ttf", 9)
+        self.width = self.screen.width
+        self.height = self.screen.height
+
+        self.font = Proportional(DEFAULT_FONT).font
+
+        self.scroll_once()
+
+        self.pixel_array = []
 
         self.start()
 
+    def scroll_once(self, direction='left'):
+        length = len(self.text)
+        start_range = []
+        if direction == 'left':
+            start_range = range(length)
+        elif direction == 'right':
+            start_range = range(length - 1, -1, -1)
+
+        for start_char in start_range:
+            for stage in range(8):
+                for col in range(8):
+                    column_data = []
+
+                    if direction == 'left':
+                        this_char = self.font[ord(self.text[start_char - 1])]
+                        next_char = self.font[ord(self.text[start_char])]
+                        if col + stage < 8:
+                            column_data += [col + 1, this_char[col + stage]]
+                        else:
+                            column_data += [col + 1, next_char[col + stage - 8]]
+                    elif direction == 'right':
+                        this_char = self.font[ord(self.text[start_char])]
+                        next_char = self.font[ord(self.text[start_char - 1])]
+                        if col >= stage:
+                            column_data += [col + 1, this_char[col - stage]]
+                        else:
+                            column_data += [col + 1, next_char[col - stage + 8]]
+
+                    print column_data
+
     def tick(self):
         self.screen.clear()
+
+        # self.screen.pixel = [[self.pixel_array[x, y + 16] for y in range(16)] for x in range(16)]
 
         self.screen.update()
         time.sleep(.001)
